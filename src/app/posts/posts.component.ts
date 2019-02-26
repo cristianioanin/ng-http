821f684df71f8e3f05fde038c1e806f6e99e9f7f
Cristian Ioanin <cristian.ioanin@gmail.com>
2019-02-26 22:47:05 +0200

    add error handling for expected application errors: not found, bad request

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../services/post.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AppError } from '../common/app-error';
+import { NotFoundError } from '../common/not-found-error';
+import { BadInput } from '../common/bad-input';
 
 @Component({
 	selector: 'app-posts',
@@ -8,35 +12,73 @@ import { PostService } from '../services/post.service';
 })
 export class PostsComponent implements OnInit {
 	posts: any[];
+	handleUnexpectedError(error: any): void {
+		alert('Unexpected error occured');
+		console.log(error);
+	}
 
 	constructor(private service: PostService) {}
 
 	ngOnInit() {
-		this.service.getPosts().subscribe((posts: Array<any>) => {
-			this.posts = posts;
-		});
+		this.service.getPosts().subscribe(
+			(posts: Array<any>) => {
+				this.posts = posts;
+			},
+			(error) => {
+				this.handleUnexpectedError(error);
+			}
+		);
 	}
 
 	createPost(input: HTMLInputElement) {
 		let post = { title: input.value };
 		input.value = '';
 
-		this.service.createPost(post).subscribe((response) => {
-			post['id'] = response['id'];
-			this.posts.unshift(post);
-		});
+		this.service.createPost(post.title).subscribe(
+			(response) => {
+				post['id'] = response['id'];
+				this.posts.unshift(post);
+			},
+			(error: AppError) => {
+				if (error instanceof BadInput) {
+					// this.form.setErrors(error.originalError);
+					alert('Bad Request: Please check your input');
+				} else {
+					this.handleUnexpectedError(error);
+				}
+			}
+		);
 	}
 
 	updatePost(post) {
-		this.service.updatePost(post).subscribe((response) => {
-			console.log(response);
-		});
+		this.service.updatePost(post).subscribe(
+			(response) => {
+				console.log(response);
+			},
+			(error: AppError) => {
+				if (error instanceof BadInput) {
+					// this.form.setErrors(error.originalError);
+					alert('Bad Request: Please check your input');
+				} else {
+					this.handleUnexpectedError(error);
+				}
+			}
+		);
 	}
 
 	deletePost(post) {
-		this.service.deletePost(post.id).subscribe((response) => {
-			let index = this.posts.indexOf(post);
-			this.posts.splice(index, 1);
-		});
+		this.service.deletePost(post.id).subscribe(
+			(response) => {
+				let index = this.posts.indexOf(post);
+				this.posts.splice(index, 1);
+			},
+			(error: AppError) => {
+				if (error instanceof NotFoundError) {
+					alert('This post has already been deleted');
+				} else {
+					this.handleUnexpectedError(error);
+				}
+			}
+		);
 	}
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { AppError } from '../common/app-error';
+import { NotFoundError } from '../common/not-found-error';
+import { BadInput } from '../common/bad-input';
 
 @Injectable({
 	providedIn: 'root'
@@ -14,16 +19,43 @@ export class PostService {
 	}
 
 	createPost(post) {
-		return this.http.post(this.url, JSON.stringify(post));
+		return this.http.post(this.url, JSON.stringify(post)).pipe(
+			map((res) => res),
+			catchError((error: HttpErrorResponse) => {
+				if (error.status === 400) {
+					return throwError(new BadInput(error));
+				} else {
+					return throwError(new AppError(error));
+				}
+			})
+		);
 	}
 
 	updatePost(post) {
-		return this.http.patch(`${this.url}/${post.id}`, JSON.stringify({ isRead: true }));
+		return this.http.patch(`${this.url}/${post.id}`, JSON.stringify({ isRead: true })).pipe(
+			map((res) => res),
+			catchError((error: HttpErrorResponse) => {
+				if (error.status === 400) {
+					return throwError(new BadInput(error));
+				} else {
+					return throwError(new AppError(error));
+				}
+			})
+		);
 
 		// this.http.put(this.url, JSON.stringify(post))
 	}
 
 	deletePost(id) {
-		return this.http.delete(`${this.url}/${id}`);
+		return this.http.delete(`${this.url}/error/${id}`).pipe(
+			map((res) => res),
+			catchError((error: HttpErrorResponse) => {
+				if (error.status === 404) {
+					return throwError(new NotFoundError());
+				} else {
+					return throwError(new AppError(error));
+				}
+			})
+		);
 	}
 }

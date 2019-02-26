909e06bcd45af4f5bb89282eefd3096cab0923e1
Cristian Ioanin <cristian.ioanin@gmail.com>
2019-02-26 20:36:13 +0200

    add HTTP request service using HttpClientModule
    use jsonplaceholder to send requests to face API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostService } from './services/post.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, PostsComponent ],
+	imports: [ BrowserModule, AppRoutingModule, HttpClientModule ],
+	providers: [ PostService ],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
